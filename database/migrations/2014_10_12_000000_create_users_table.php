8eddbeb8ce6f6c03cbaed839dada9014af0b8b92
kabi097 <paweljanwiczynski@gmail.com>
2019-05-22 17:30:02 +0200

    ref_code support

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            // $table->engine = 'InnoDB'; 
+            $table->increments('id')->index();
             $table->boolean('active')->default(true);
             $table->boolean('banned')->default(false);
             $table->boolean('notifable_email')->default(false);
@@ -23,20 +24,26 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('avatar')->nullable();
-            $table->integer('votes')->unsigned()->nullable()->default(0);
             $table->string('steam_id', 255)->nullable();
             $table->string('facebook_id', 255)->nullable();
             $table->string('google_id', 255)->nullable();
             $table->text('geo')->nullable();
             $table->string('lang', 2)->nullable()->default('pl');
-            $table->integer('votes')->unsigned()->nullable()->default(12);
+            $table->integer('votes')->unsigned()->nullable();
             $table->unsignedInteger('ref')->nullable();
-            $table->foreign('ref')->references('id')->on('comments')->onDelete("set null");
+            $table->foreign('ref')->references('id')->on('users')->onDelete("set null");
             $table->boolean('ref_status')->nullable()->default(false);
             $table->string('ref_code')->unique()->default('text');
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
         });
+        DB::unprepared('
+        CREATE TRIGGER add_ref_code BEFORE INSERT ON users 
+        FOR EACH ROW 
+        BEGIN 
+        SET NEW.ref_code = CONCAT("GC_", NEW.id); 
+        END;
+    ');
     }
 
     /**
